17026d5c22289e4c503f7ef28aaafa92865897ec
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-14 11:07:24 +0900

    Script to interact with the NFT contract. Loading the contract address that it was deployed to

---
+++ scripts/interact.js
@@ -0,0 +1,11 @@
+const hre = require("hardhat");
+
+async function main() {
+    const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const [owner, addr1] = await hre.ethers.getSigners();
+
+    const MyNFT = await hre.ethers.getContractFactory("MyNFT");
+    const myNFT = MyNFT.attach(CONTRACT_ADDRESS);
+
+    console.log("Connected to contract at:", CONTRACT_ADDRESS);
+}
