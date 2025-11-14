f5860e06bcba47e0820e03205a932335db02fde1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-14 10:57:20 +0900

    Updating the deployment script to work with the latest OpenZepellin dependencies

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,20 @@
+const hre = require("hardhat");
+
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    console.log("Deploying contracts with account:", deployer.address);
+    const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying contract with account:", deployer.address);
 
-    const MyNFT = await ethers.getContractFactory("MyNFT");
+    const MyNFT = await hre.ethers.getContractFactory("MyNFT");
+
+    const name = "My NFT";
+    const symbol = "MNFT";
     const baseURI = "https://example.com/metadata/";
-    const myNFT = await MyNFT.deploy("MyNFT", "MNFT", baseURI);
 
-    await myNFT.dpeloyed();
-    console.log("MyNFT deployed to:", myNFT.address);
+    const myNFT = await MyNFT.deploy(name, symbol, baseURI);
+
+    await myNFT.waitForDeployment();
+
+    console.log("MyNFT deployed to:", await myNFT.getAddress());
 }
 
 main().catch((error) => {
